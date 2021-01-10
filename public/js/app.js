2a4ca581150243f9c236f795ac9ca41fd04f2493
tomkeane07 <tomkeane07@gmail.com>
2021-01-10 00:18:36 +0000

    ajax refactoring

--- public/js/app.js
+++ public/js/app.js
@@ -3794,55 +3794,63 @@ module.exports = {
 $(document).ready(function () {
   //ADD
   $('#ajaxSubmit_add').click(function (e) {
-    $.ajaxSetup({
-      headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
-    });
+    csrf_setup();
     e.preventDefault();
-    $.ajax({
-      url: $('#add_item_form').attr('action'),
-      method: 'POST',
-      data: {
-        'item_name': $('#add_item_name').val()
-      },
-      success: function success(result) {
-        console.log("ajax success");
-        console.log(result);
-        window.location.href = "/dashboard";
-      },
-      error: function error(result, request) {
-        console.log("ajax failure");
-        console.log(result);
-      }
-    });
+    add_item_call();
   }); //EDIT
 
   $('#ajaxSubmit_edit').click(function (e) {
-    $.ajaxSetup({
-      headers: {
-        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-      }
-    });
+    csrf_setup();
     e.preventDefault();
-    $.ajax({
-      url: $('#edit_item_form').attr('action'),
-      method: 'POST',
-      data: {
-        'item_name': $('#edit_item_name').val()
-      },
-      success: function success(result) {
-        console.log("ajax success");
-        console.log(result);
-        window.location.href = "/dashboard";
-      },
-      error: function error(result, request) {
-        console.log("ajax failure");
-        console.log(result);
-      }
-    });
+    edit_item_call();
   });
 });
+
+var add_item_call = function add_item_call() {
+  $.ajax({
+    url: $('#add_item_form').attr('action'),
+    method: 'POST',
+    data: {
+      'item_name': $('#add_item_name').val()
+    },
+    success: function success(result) {
+      console.log("ajax success");
+      console.log(result);
+      window.location.href = "/dashboard";
+    },
+    error: function error(result, request) {
+      console.log("ajax failure");
+      console.log(result);
+    }
+  });
+};
+
+var edit_item_call = function edit_item_call() {
+  $.ajax({
+    url: $('#edit_item_form').attr('action'),
+    method: 'POST',
+    data: {
+      'item_name': $('#edit_item_name').val()
+    },
+    success: function success(result) {
+      console.log("ajax success");
+      console.log(result);
+      window.location.href = "/dashboard";
+    },
+    error: function error(result, request) {
+      console.log("ajax failure");
+      console.log(result);
+    }
+  });
+};
+
+var csrf_setup = function csrf_setup() {
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+};
 
 /***/ }),
 
--- public/js/item_ajax_handler.js
+++ public/js/item_ajax_handler.js
@@ -1,40 +1,40 @@
 $(document).ready(function(){
     //ADD
     $('#ajaxSubmit_add').click(function(e){
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
+        csrf_setup();
         e.preventDefault();
-        $.ajax({
-            url: $('#add_item_form').attr('action'),
-            method: 'POST',
-            data: {
-                'item_name': $('#add_item_name').val()
-            },
-
-            success: function(result){
-                console.log("ajax success");
-                console.log(result);
-                window.location.href = "/dashboard";
-            },
-            error: function(result, request){
-                console.log("ajax failure");
-                console.log(result);
-            }
-        });
+        add_item_call();
     });
 
     //EDIT
     $('#ajaxSubmit_edit').click(function(e){
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
+        csrf_setup();
         e.preventDefault();
-        $.ajax({
+        edit_item_call();
+    });
+});
+
+
+var add_item_call = function() { $.ajax({
+    url: $('#add_item_form').attr('action'),
+    method: 'POST',
+    data: {
+        'item_name': $('#add_item_name').val()
+    },
+
+    success: function(result){
+        console.log("ajax success");
+        console.log(result);
+        window.location.href = "/dashboard";
+    },
+    error: function(result, request){
+        console.log("ajax failure");
+        console.log(result);
+    }
+});}
+
+
+var edit_item_call = function () { $.ajax({
             url: $('#edit_item_form').attr('action'),
             method: 'POST',
             data: {
@@ -50,6 +50,10 @@ $(document).ready(function(){
                 console.log("ajax failure");
                 console.log(result);
             }
-        });
-    });
-});
+});}
+
+var csrf_setup = function(){ $.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});}
